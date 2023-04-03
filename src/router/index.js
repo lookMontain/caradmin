b3d8dd3085cf78078cf723e6d8704fb11a131c47
hehong <wohev5@163.com>
2023-04-03 17:45:10 +0800

    优化登陆后进去数据总览

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,7 @@ export const constantRoutes = [
   {
     path: '/',
     component: Layout,
-    redirect: '/dashboard',
+    redirect: '/dataOverview',
     children: [{
       path: 'dashboard',
       name: 'Dashboard',
